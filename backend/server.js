c4f03f9c89a1c8957ef385cab4783db91ed2cfdf
kphan <kphan@hotmail.fr>
2025-09-19 17:52:29 +0200

    add server.js

--- backend/server.js
+++ backend/server.js
@@ -44,4 +44,6 @@ server.on('listening', () => {
   console.log('Listening on ' + bind);
 });
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
