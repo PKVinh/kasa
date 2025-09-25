b00abc2e5b99b11ebe27950552aebe96fc647828
kphan <kphan@hotmail.fr>
2025-09-25 17:26:46 +0200

    add new build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/kasa/', // 👈 ou '/<nom-du-repo>/' si ton repo n'est pas en user page
+  build: {
+    outDir: '../docs', // build ira directement dans /docs
+    emptyOutDir: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
